62b4c16b7a192175f9f4d8d0779b389339d409a6
chat <omarabdelkhalek12@gmail.com>
2025-07-24 04:59:04 +0300

    Add files via upload

---
+++ CODEFORCES PROBLEM 2126 A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+    while(t){
+        string x;
+        cin >> x;
+        int n = x.size();
+        int array[n];
+        for(int i = 0;i<n;i++){
+            array[i] = x[i] - '0';
+        }
+        int minArray = *min_element(array,array+n);
+        cout << minArray << endl;
+        t--;
+    }
+}
